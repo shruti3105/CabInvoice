7e08cdc111d9440e423f5304f2773763e0cb28ba
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-13 10:40:14 +0530

    uploading project files

---
+++ lib/src/test/java/CabInvoice/InvoiceServiceTest.java
@@ -0,0 +1,24 @@
+package CabInvoice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25,fare,0.0);
+	}
+	
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(5,fare,0.0);
+	}
+}
