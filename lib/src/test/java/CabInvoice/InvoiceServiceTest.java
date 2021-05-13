f300a2f1296bc1bda7b70ad19238c47f48dc4f8c
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-13 12:56:13 +0530

    UC3

--- lib/src/test/java/CabInvoice/InvoiceServiceTest.java
+++ lib/src/test/java/CabInvoice/InvoiceServiceTest.java
@@ -22,11 +22,20 @@ public class InvoiceServiceTest {
 		Assert.assertEquals(5,fare,0.0);
 	}
 	
-	@Test
+	/*@Test
 	public void givenMultipleRides_ShowReturnTotalFare() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = {new Ride(2.0,5), new Ride(0.1,1)};
 		double fare = invoiceGenerator.calculateFare(rides);
 		Assert.assertEquals(30,fare,0.0);
+	}*/
+	
+	@Test
+	public void givenMultipleRides_ShowReturnInvoiceSummary() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = {new Ride(2.0,5), new Ride(0.1,1)};
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,5);
+		Assert.assertEquals(expectedInvoiceSummary,summary);
 	}
 }
