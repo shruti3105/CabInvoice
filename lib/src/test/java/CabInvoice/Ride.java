5a0f46f015b1d5900c799448d66657967a0b3b0f
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-13 11:04:23 +0530

    UC2

---
+++ lib/src/test/java/CabInvoice/Ride.java
@@ -0,0 +1,12 @@
+package CabInvoice;
+
+public class Ride {
+	public final int time;
+	public double distance;
+	
+	public Ride(double distance,int time) {
+		this.distance=distance;
+		this.time=time;
+	}
+
+}
